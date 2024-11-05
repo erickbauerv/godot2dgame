c95047fb6b14d47f1042d5917130efa053e75e7f
erickbauer <contatoerickbauer@gmail.com>
2024-11-05 16:20:49 -0300

    Added sounds

--- scripts/Coin.cs
+++ scripts/Coin.cs
@@ -4,9 +4,18 @@ public partial class Coin : Area2D
 {
 	public void _on_body_entered(Node2D body)
 	{
-		GameManager gameManager = GetNode<GameManager>("%GameManager");
+		var gameManager = GetNode<GameManager>("%GameManager");
+		var audioStreamPlayer = GetNode<AudioStreamPlayer2D>("PickupSound");
+		var collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
 
+		collisionShape.QueueFree();
+		Visible = false;
+
+		audioStreamPlayer.Play();
 		gameManager.AddPoint();
+	}
+
+	public void _on_pickup_sound_finished(){
 		QueueFree();
 	}
 }
